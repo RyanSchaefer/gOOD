e0dfc2dab2a06231e2cacd3c72766c71bb4007b2
ryanschaefer <contact.devsquid@gmail.com>
2019-10-21 18:56:27 -0400

    added more tests

--- src/edu/cs3500/spreadsheets/model/IWorksheet.java
+++ src/edu/cs3500/spreadsheets/model/IWorksheet.java
@@ -7,30 +7,20 @@ public interface IWorksheet {
 
   /**
    * Returns the cell at the given coordinates or null if there is no cell there
-   * @param col the column of the cell
-   * @param row the row of the cell
+   * @param col the column of the cell, 0 indexed
+   * @param row the row of the cell, 0 indexed
    * @return the cell or null
    */
   ICell getCellAt(int col, int row);
 
   /**
-   * Returns the name of this worksheet
-   * @return the name of the worksheet
-   */
-  String getSheetName();
-
-  /**
    * Changes the cell at the given coordinates to contain the given sexp
-   * @param col the column of the cell
-   * @param row the row of the cell
+   * @param col the column of the cell, 0 indexed
+   * @param row the row of the cell, 0 indexed
    * @param sexp the sexp to insert
+   * @throws IllegalArgumentException Sexp is null
    */
-  void changeCellAt(int col, int row, Sexp sexp);
+  void changeCellAt(int col, int row, Sexp sexp) throws IllegalArgumentException;
 
-  /**
-   * Change the sheets name to the given name
-   * @param name the new name
-   */
-  void changeSheetName(String name);
 
 }
--- test/TestModel.java
+++ test/TestModel.java
@@ -1,6 +1,7 @@
+import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals
+import static org.junit.Assert.assertEquals;
 
 import java.io.FileReader;
 import java.io.IOException;
@@ -8,7 +9,9 @@ import java.io.IOException;
 import edu.cs3500.spreadsheets.model.BasicWorksheet;
 import edu.cs3500.spreadsheets.model.IWorksheet;
 import edu.cs3500.spreadsheets.model.WorksheetReader;
+import edu.cs3500.spreadsheets.sexp.SList;
 import edu.cs3500.spreadsheets.sexp.SNumber;
+import edu.cs3500.spreadsheets.sexp.SSymbol;
 
 abstract public class TestModel {
 
@@ -30,6 +33,47 @@ abstract public class TestModel {
   public void getCellValue() {
     IWorksheet sheet = model("test_worksheets/test1.gOOD");
     assertEquals(new SNumber(3), sheet.getCellAt(0, 0).getValue());
+    assertEquals(new SNumber(4), sheet.getCellAt(1, 0).getValue());
+    assertEquals(new SNumber(9), sheet.getCellAt(2, 0).getValue());
+    assertEquals(new SNumber(12), sheet.getCellAt(3, 0).getValue());
+    assertEquals(new SList(
+            new SSymbol("PRODUCT"),
+            new SList(
+                    new SSymbol("SUB"),
+                    new SSymbol("C1"),
+                    new SSymbol("A1")),
+            new SList(
+                    new SSymbol("SUB"),
+                    new SSymbol("C1"),
+                    new SSymbol("A1"))), sheet.getCellAt(0,1).getValue());
+    assertEquals(new SList(
+            new SSymbol("PRODUCT"),
+            new SList(
+                    new SSymbol("SUB"),
+                    new SSymbol("D1"),
+                    new SSymbol("B1")),
+            new SList(
+                    new SSymbol("SUB"),
+                    new SSymbol("D1"),
+                    new SSymbol("B1"))), sheet.getCellAt(0, 2).getValue());
+    assertEquals(new SList(
+            new SSymbol("<"),
+            new SSymbol("A3"),
+            new SNumber(10)), sheet.getCellAt(1, 2).getValue());
+  }
+
+  @Test
+  public void changeCellValue() {
+    IWorksheet sheet = model("test_worksheets/test1.gOOD");
+    assertEquals(new SNumber(3), sheet.getCellAt(0, 0).getValue());
+    sheet.changeCellAt(0, 0, new SNumber(5));
+    assertEquals(new SNumber(5), sheet.getCellAt(0, 0).getValue());
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void nullSexpThrowsException() {
+    IWorksheet sheet = model("test_worksheets/test.gOOD");
+    sheet.changeCellAt(0, 0, null);
   }
 
 }
