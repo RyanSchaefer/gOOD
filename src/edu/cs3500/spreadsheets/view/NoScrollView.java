5e58b5da794f1bc98db9c8b88d6f62d36f13d3b9
ryanschaefer <contact.devsquid@gmail.com>
2019-11-04 18:24:00 -0500

    We have something I think

--- src/edu/cs3500/spreadsheets/view/NoScrollView.java
+++ src/edu/cs3500/spreadsheets/view/NoScrollView.java
@@ -1,8 +1,12 @@
 package edu.cs3500.spreadsheets.view;
 
 import java.awt.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.swing.*;
 
@@ -14,13 +18,41 @@ public class NoScrollView extends JFrame implements IView {
   IWorksheet model;
   Frame screen;
 
-  public NoScrollView(IWorksheet model) {
-    super();
-    this.setTitle("gOOD");
-    this.setSize(500, 500);
-    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  Map<Coord, CellView> cells = new HashMap<>();
 
+  private int minWCell = 1;
+  private int maxWCell = 10;
+  private int minHCell = 1;
+  private int maxHCell = 20;
+
+  public NoScrollView(IWorksheet model) throws IOException {
+    super();
+    this.model = model;
+    this.setTitle("gOOD");
     this.setLayout(new GridBagLayout());
+    this.setSize(CellView.CELL_SIZE.width * 10, CellView.CELL_SIZE.height * 20);
+    this.addComponentListener(new ComponentAdapter() {
+      @Override
+      public void componentResized(ComponentEvent e) {
+        int oldMaxWCell = maxWCell;
+        int oldMaxHCell = maxHCell;
+        super.componentResized(e);
+        Dimension newSize = e.getComponent().getSize();
+        maxHCell = (newSize.height / CellView.CELL_SIZE.height);
+        maxWCell = (newSize.width / CellView.CELL_SIZE.width);
+        try {
+          if (oldMaxHCell != maxHCell || oldMaxWCell != maxWCell) {
+            removeCells(oldMaxHCell, maxHCell, oldMaxWCell, maxWCell);
+            renderSpreadsheet();
+          }
+        } catch (IOException ex) {
+
+        }
+      }
+    });
+    renderSpreadsheet();
+    this.pack();
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
   }
 
   @Override
@@ -28,8 +60,46 @@ public class NoScrollView extends JFrame implements IView {
 
   }
 
+
   @Override
   public void renderSpreadsheet() throws IOException {
+    GridBagConstraints c = new GridBagConstraints();
+    c.fill = GridBagConstraints.BOTH;
+    for (int x = minWCell; x < maxWCell; x++) {
+      for (int y = minHCell; y < maxHCell; y++) {
+        c.gridx = x - 1;
+        c.gridy = y - 1;
+        Coord coord = new Coord(x, y);
+        CellView view = new CellView(coord, model.evaluateCellAt(x, y));
+        this.add(view, c);
+        cells.put(coord, view);
+      }
+    }
+  }
 
+  private void removeCells(int oldMaxH, int newMaxH, int oldMaxW, int newMaxW) {
+    for (int x = minWCell; x < oldMaxW; x++) {
+      for (int y = minHCell; y < newMaxH; y++) {
+        Coord coord = new Coord(x, y);
+        if (cells.containsKey(coord)) {
+          this.remove(cells.get(coord));
+          cells.remove(coord);
+        }
+      }
+    }
+    for (int y = minHCell; y < oldMaxH; y++) {
+      for (int x = minWCell; x < oldMaxW; x++) {
+        Coord coord = new Coord(x, y);
+        if (cells.containsKey(coord)) {
+          this.remove(cells.get(coord));
+          cells.remove(coord);
+        }
+      }
+    }
+  }
+
+  @Override
+  public void makeVisible() {
+    this.setVisible(true);
   }
 }
